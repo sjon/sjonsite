6c80fb1d1097edf3feb0fb2ac56021f51567f601
sjonscom <sjonscom@039ce67a-491d-0410-970c-9965483c9e75>
2006-09-13 05:50:16 +0000

    importing sjonsite structure, including some files

---
+++ htdocs/index.php
@@ -0,0 +1,20 @@
+<?php
+
+	/**
+	 * SjonSite - Frontend Controller
+	 *
+	 * @author Sjon <sjonscom@gmail.com>
+	 * @package SjonSite
+	 * @version $Id$
+	 */
+
+	require_once '../include/config.php';
+
+	/**
+	 *
+	 */
+	final class SjonSite_FrontendController extends SjonSite_Main implements SjonSite_Controller {
+
+	}
+
+?>
---
+++ include/config.php
@@ -0,0 +1,18 @@
+<?php
+
+	/**
+	 * SjonSite - Configuration File
+	 *
+	 * @author Sjon <sjonscom@gmail.com>
+	 * @package SjonSite
+	 * @version $Id$
+	 */
+
+	define ('SJONSITE_URL', '');
+	define ('SJONSITE_DSN', '');
+	define ('SJONSITE_BASE', dirname(__FILE__));
+	define ('SJONSITE_VAR', realpath(SJONSITE_BASE . '/../var'));
+
+	require_once SJONSITE_BASE . '/library.php');
+
+?>
