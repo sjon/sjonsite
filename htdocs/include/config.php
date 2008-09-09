cbeda63d1f9c4f466c0040107bcee36da5e23ee4
sjonscom <sjonscom@039ce67a-491d-0410-970c-9965483c9e75>
2008-09-09 20:49:08 +0000

    Added some of the new framework, still lots to do...

--- htdocs/include/config.php
+++ htdocs/include/config.php
@@ -1,7 +1,7 @@
 <?php
 
 	/**
-	 * Sjonsite -
+	 * Sjonsite - Configuration
 	 *
 	 * @author Sjon <sjonscom@gmail.com>
 	 * @package Sjonsite
@@ -9,5 +9,54 @@
 	 * @license Mozilla Public License 1.1
 	 * @version $Id$
 	 */
+
+	/**
+	 * Start of request
+	 *
+	 * @var float
+	 */
+	define ('SJONSITE_START', microtime(true));
+
+	/**
+	 * Path to the include directory
+	 *
+	 * @var string
+	 */
+	define ('SJONSITE_INCLUDE', dirname(__FILE__));
+
+	/**
+	 * PDO connection string
+	 *
+	 * @var string
+	 */
+	define ('SJONSITE_PDO_DSN', '');
+
+	/**
+	 * PDO connection username
+	 *
+	 * @var string
+	 */
+	define ('SJONSITE_PDO_USER', '');
+
+	/**
+	 * PDO connection password
+	 *
+	 * @var string
+	 */
+	define ('SJONSITE_PDO_PASS', '');
+
+	/**
+	 * PDO table prefix
+	 *
+	 * @var string
+	 */
+	define ('SJONSITE_PDO_PREFIX', 'sjonsite_');
+
+	/**
+	 * Enter description here...
+	 *
+	 * @var
+	 */
+	define ('SJONSITE_', '');
 
 ?>
